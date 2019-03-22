bc8030e13f9b54f6b5be67b41cfb6ab5a3665076
Rafael <r.tomaszewski.a5@gmail.com>
2019-03-22 14:26:32 +0100

    Three inputs.

--- js/test.jsx
+++ js/test.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Icon, Input} from 'semantic-ui-react';
+import {Icon, Input, Dropdown, Button} from 'semantic-ui-react';
+import '../css/style.css';
+
+
 
 
 function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
-
-
 
 class Hello extends React.Component {
     render(){
@@ -27,12 +28,47 @@ class InputDisable extends React.Component {
             iconName: 'balance scale',
             iconNameFirst: 'unlock',
             iconNameSecond: 'unlock',
+            iconNameThird: 'unlock',
             isDisabledFirst: '',
-            isDisabledSecond: ''
+            isDisabledSecond: '',
+            isDisabledThird: '',
+            num1: '',
+            num2:'',
+            result:''
         }
     }
 
+    handleChange1 = (e) => {
+        this.setState({
+            num1: e.currentTarget.value
+        })
+    }
+
+    handleChange2 = (e) => {
+        this.setState({
+            num2: e.currentTarget.value
+        })
+    }
+
+
     render(){
+
+        const options = [
+            { key: 'rest value 1', text: 'rest value 1', value: 'rest value 1' },
+            { key: 'best value 2', text: 'best value 2', value: 'best value 2' },
+            { key: 'test value 3', text: 'test value 3', value: 'test value 3' },
+            { key: 'test value 4', text: 'test value 4', value: 'test value 4' },
+            { key: 'test value 5', text: 'test value 5', value: 'test value 5' },
+            { key: 'test value 6', text: 'test value 6', value: 'test value 6' },
+            { key: 'test value 7', text: 'test value 7', value: 'test value 7' },
+            { key: 'test value 8', text: 'test value 8', value: 'test value 8' },
+            { key: 'test value 9', text: 'test value 9', value: 'test value 9' },
+            { key: 'test value 10', text: 'test value 10', value: 'test value 10' },
+            { key: 'test value 11', text: 'test value 11', value: 'test value 11' },
+            { key: 'test value 12', text: 'test value 12', value: 'test value 12' }
+        ]
+
+
         return (
             <div>
                 <center><Icon name={this.state.iconName} size={'massive'}/></center>
@@ -40,6 +76,7 @@ class InputDisable extends React.Component {
                 <br />
                 
                 <Input name='first'
+                       onChange={this.handleChange1} value={this.state.num1}
                        disabled={this.state.isDisabledFirst}
                        icon={<Icon name={this.state.iconNameFirst} link
                                    onClick={()=> {
@@ -53,9 +90,9 @@ class InputDisable extends React.Component {
                             />}
                 />
 
-                <br /><br />
 
                 <Input name='second'
+                       onChange={this.handleChange2} value={this.state.num2}
                        disabled={this.state.isDisabledSecond}
                        icon={<Icon name={this.state.iconNameSecond} link
                                    onClick={()=> {
@@ -68,6 +105,42 @@ class InputDisable extends React.Component {
 
                        />}
                 />
+
+                <Input name='result'
+                       value={isNaN(Number(this.state.num1)) || isNaN(Number(this.state.num2)) ? 'Nie liczba' : Number(this.state.num1)*Number(this.state.num2)}
+                       disabled={this.state.isDisabledThird}
+                       icon={<Icon name={this.state.iconNameThird} link
+                                   onClick={()=> {
+                                       this.setState({
+                                           iconNameThird: this.state.iconNameThird === 'unlock'  ? 'lock' : 'unlock',
+                                           isDisabledThird: this.state.isDisabledThird === '' ? 'disabled' : ''
+                                       }), console.log('Result')
+
+                                   }}
+
+                       />}
+                />
+
+                <br /><br />
+
+                <center>
+
+                    <Button style={{color: 'red'}}>Click Here</Button>
+
+
+                    <br /><br />
+
+                    <Dropdown
+                        placeholder='Select...'
+                        icon='search'
+                        search
+                        selection
+                        options={options}
+                    />
+
+                </center>
+
+                <h1>{isNaN(Number(this.state.num1)) || isNaN(Number(this.state.num2)) ? 'Nie liczba' : Number(this.state.num1)*Number(this.state.num2)}</h1>
 
             </div>
         )
